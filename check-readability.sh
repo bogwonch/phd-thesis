d514d71a949370752704bce86a3e1e5a980c6678
Joseph Hallett <bogwonch@bogwonch.net>
2017-06-26 15:57:01 +0100

    adds weights to readability so I know where to focus to get in up

--- check-readability.sh
+++ check-readability.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
+detex="detex -e lstlisting,figure,table,tabular,quote"
+
+total_word_count=$($detex chapters/*.tex | wc -w)
 for f in chapters/*.tex; do 
+	text=$($detex $f)
+	words=$(wc -w <<<"$text")
 	echo -ne "$f\t"
-	detex $f | style | awk '/Flesch/ {print $3}'
+	fi=$((style <<< "$text") | awk '/Flesch/ {print $3}' | awk -F/ '{print $1}')
+	echo -ne "$fi\t"
+	python -c $"
+weight=($fi - 60)*($words.0 / $total_word_count)
+if weight >= 0:
+	print('\033[32m%0.2f\033[0m' % weight)
+else:
+	print('\033[31m%0.2f\033[0m' % (0-weight))
+"
 done | column -t
 echo
-detex chapters/*.tex | style | awk '/Flesch/ {print "Overall: " $3}'
+$detex chapters/*.tex | style | awk '/Flesch/ {print "Overall: " $3}'
 echo
